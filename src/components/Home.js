cf2b19f2c70478ae136f582ad51d62048c229952
Arjun-Gopinath <40708507+Arjun-Gopinath@users.noreply.github.com>
2020-08-27 22:09:51 +0530

    desc updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,15 +3,31 @@ import './css/Home.css'
 import propic from '../images/propic.png'
 
 const Home = () => {
+    var i = 0;
+    var txt = 'If you are seeing this you have entered ARJUN G\'s webpage, a BTech graduate and an aspiring web-developer.'; 
+    var speed = 100; /* The speed/duration of the effect in milliseconds */
+    
+    function type() {
+      if (i < txt.length) {
+        document.getElementById("demo").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(type, speed);
+      }
+    }
+
     return (
-        <div className="homecontainer">
-            <img className="Propic" src={propic} alt="Propic"/>
-            <div className="homedesc">
-                <h1>Arjun G</h1>
+        <div className="homecontainer" onLoad={() => type()}>
+            <div className="Propic" >
+                <img src={propic} alt="Propic"/>
             </div>
-        
+            <div className="homedesc">
+                <h1 id="demo"></h1>
+            </div>
+            
         </div>
     )
 }
 
 export default Home
+
+
