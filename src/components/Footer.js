a35b6fdab01594e2bb09b36e4023b0e897da3f74
Arjun-Gopinath <40708507+Arjun-Gopinath@users.noreply.github.com>
2020-08-30 21:19:45 +0530

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import {DiGithubBadge} from 'react-icons/di'
+import {FaLinkedinIn,FaInstagram} from 'react-icons/fa'
+import './css/Footer.css'
 
 const Footer = () => {
     return (
-        <div>
-            <>
+        <div className="footer">
             <h2>Footer Ahead</h2>
-            </>
+            <div className="flinks">
+                <Fcard
+                icon = {<DiGithubBadge/>}
+                links = "https://github.com/Arjun-Gopinath"
+                />
+                <Fcard
+                icon = {<FaLinkedinIn/>}
+                links = "https://www.linkedin.com/in/arjun-gopinath-b3b57b1a1/"
+                />
+                <Fcard
+                icon = {<FaInstagram/>}
+                links = "https://www.instagram.com/arju_g/"
+                />
+            </div>
+        </div>
+    )
+}
+
+const Fcard = ({icon,links}) => {
+    return(
+        <div className="fcard">
+            <a href={links}>{icon}</a>
         </div>
     )
 }
