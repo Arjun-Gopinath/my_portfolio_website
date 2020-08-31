77d52d18ac191f7fb3ebcd59dfd7834406cd4f96
Arjun-Gopinath <40708507+Arjun-Gopinath@users.noreply.github.com>
2020-08-31 18:52:05 +0530

    commit before deploy

---
+++ server.js
@@ -0,0 +1,13 @@
+var PORT = process.env.PORT || 5000;
+var express = require("express");
+var app = express();
+
+app.use(express.static('src'));
+
+app.get('/',function(req,res){
+    res.render('public/index');
+});
+
+app.listen(process.env.PORT || 5000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
