e8da791be9fe5f6546d3349546711aaadd528e27
Arjun-Gopinath <40708507+Arjun-Gopinath@users.noreply.github.com>
2020-08-31 12:59:47 +0530

    updated deploy

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(express.static('src/components/css'));
 
 app.get('/',function(req,res){
-    res.render('public/index.html');
+    res.render('index.html');
 });
 
 app.listen(process.env.PORT || 5000, function(){
