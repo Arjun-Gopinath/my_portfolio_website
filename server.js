2b39c6f6179ac1ea2fcb5dfaf48793f81baf1b9f
Arjun-Gopinath <40708507+Arjun-Gopinath@users.noreply.github.com>
2020-08-31 22:00:01 +0530

    updated deploy

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ var app = express();
 app.use(express.static('src/static'));
 
 app.get('/',function(req,res){
-    res.render('public','index');
+    res.render('index');
 });
 
-app.listen(process.env.PORT || 5000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
