aee5b703d753d4fb4168f337ddf54070649dbb4b
Arjun-Gopinath <40708507+Arjun-Gopinath@users.noreply.github.com>
2020-08-31 11:50:18 +0530

    updated deploy

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ var PORT = process.env.PORT || 5000;
 var express = require("express");
 var app = express();
 
-var http = require("http");
-var server = http.Server(app)
-
 app.use(express.static(path.join(__dirname,'src')));
 
 app.get('/',function(req,res){
