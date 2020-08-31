287144096d0a07d5f5e8fe97a854f40e60f40a82
Arjun-Gopinath <40708507+Arjun-Gopinath@users.noreply.github.com>
2020-08-31 10:59:15 +0530

    commit before deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.PORT || 8080
 app.use(express.static(__dirname));
 app.get("/",function(req,res){
-    res.render("public/index");
+    res.render("public/index.html");
 })
 
 app.listen(port,function(){
