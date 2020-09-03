c4acb51ba069cf6a7939e05e9f37bd0ca1c8001d
Arjun-Gopinath <40708507+Arjun-Gopinath@users.noreply.github.com>
2020-09-04 01:04:35 +0530

    css updates

--- src/views/About.js
+++ src/views/About.js
@@ -1,24 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 import '../static/css/About.css'
 
-const About = () => {
-    return (
-        <div className="abtform">
-            <form action="mailto:arjungopinath06@gmail.com" 
-            method="POST"enctype="multipart/form-data" 
-            className="EmailForm">
-                <div className="subrow">
-                    <h1>Name:</h1>
-                    <input type="text" size="29" className="ContactName"/>
-                </div>
-                <div className="subrow">
-                    <h1>Message:</h1> 
-                    <textarea className="ContactComment" rows="6" cols="29"> </textarea>
-                </div>
-                <input type="submit" value="Submit" className="btn"/>
-            </form>
-        </div>
-    )
+class About extends Component{
+    state = {
+        name :'',
+        content:'',
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id] :e.target.value
+        })
+    }
+
+    handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(this.state);
+    }
+    
+    render(){
+        return (
+            <div className="abtform">
+                <h1>Let me know what you think about my works, including this website!!</h1>
+                <form action="mailto:arjungopinath06@gmail.com" 
+                method="POST"enctype="multipart/form-data"
+                onSubmit = {this.handleSubmit} 
+                className="EmailForm">
+                    <div className="subrow">
+                        <label htmlFor="name">Name :</label>
+                        <input type="text" size="29" 
+                        className="ContactName" id="name"
+                        onChange={this.handleChange}/>
+                    </div>
+                    <div className="subrow">
+                        <label htmlFor="content">Message :</label> 
+                        <textarea className="ContactComment" 
+                        rows="6" cols="29" id ="content" 
+                        onChange={this.handleChange}></textarea>
+                    </div>
+                    <input type="submit" value="Submit" className="btn"/>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default About
